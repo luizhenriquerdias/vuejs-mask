e29a419e1f6507eedbdbf99e4a9769772e946f25
Alberto Vieira de Lima <albertovieiradelima@gmail.com>
2020-11-24 10:59:18 -0300

    Update config.js
    Adding more tokens from mask to resolve problems of the numbers and letters masks

--- src/config.js
+++ src/config.js
@@ -1,4 +1,8 @@
 export default {
 	default: ['(##) ####-####', '(##) #####-####'],
-	token: { '#': { pattern: /\d/ } }
+	token: {
+		'#': { pattern: /\d/ }, // Only numbers
+		'N': { pattern: /[\d\a-z\A-Z]/ }, // Numbers and letters
+		'A': { pattern: /[\a-z\A-Z]/ } // Only letters
+	}
 };
